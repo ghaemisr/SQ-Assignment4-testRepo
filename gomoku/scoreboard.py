3876033cfef0cfdcf7d81c16e0cbef651127bf75
Sara Ghaemi <ghaemi.sr@gmail.com>
2019-02-21 20:22:48 -0700

    RED, wrote test cases for scoreboard class
    Wrote test cases for scoreboard class and I expect them to fail.

--- gomoku/scoreboard.py
+++ gomoku/scoreboard.py
@@ -1,7 +1,11 @@
 class Scoreboard:
 
     def __init__(self):
+        self.result = {'black': 0, 'white': 0, 'tie': 0}
         pass
 
     def set_new_result(self, winner):
+        pass
+
+    def reset_scoreboard(self):
         pass
