993fc00e3672df7a5343209cd551d1f9ceb495b7
Changyuan Lin <linchangyuan1996@gmail.com>
2019-02-24 01:44:03 -0700

    [RED] wrote test cases for gamelogic class

--- test/gameLogicTest.py
+++ test/gameLogicTest.py
@@ -1,9 +1,10 @@
 import unittest
 from gomoku.gameLogic import GameLogic
 
+
 class TestGameLogic(unittest.TestCase):
     def setUp(self):
-        self.gl=GameLogic()
+        self.gl = GameLogic()
 
     def test_check_horizontal(self):
         chessboardMatrix = [
@@ -26,12 +27,12 @@ class TestGameLogic(unittest.TestCase):
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1]]
-        self.assertEqual(True,self.gl.check_horizontal(chessboardMatrix,0,0))
-        self.assertEqual(True,self.gl.check_horizontal(chessboardMatrix,0,1))
-        self.assertEqual(True,self.gl.check_horizontal(chessboardMatrix,0,4))
-        self.assertEqual(True,self.gl.check_horizontal(chessboardMatrix,18,14))
-        self.assertEqual(True,self.gl.check_horizontal(chessboardMatrix,18,15))
-        self.assertEqual(True,self.gl.check_horizontal(chessboardMatrix,18,18))
+        self.assertEqual(True, self.gl.check_horizontal(chessboardMatrix, 0, 0))
+        self.assertEqual(True, self.gl.check_horizontal(chessboardMatrix, 0, 1))
+        self.assertEqual(True, self.gl.check_horizontal(chessboardMatrix, 0, 4))
+        self.assertEqual(True, self.gl.check_horizontal(chessboardMatrix, 18, 14))
+        self.assertEqual(True, self.gl.check_horizontal(chessboardMatrix, 18, 15))
+        self.assertEqual(True, self.gl.check_horizontal(chessboardMatrix, 18, 18))
 
     def test_check_vertical(self):
         chessboardMatrix = [
@@ -54,13 +55,12 @@ class TestGameLogic(unittest.TestCase):
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
-        self.assertEqual(True,self.gl.check_vertical(chessboardMatrix,0,0))
-        self.assertEqual(True,self.gl.check_vertical(chessboardMatrix,1,0))
-        self.assertEqual(True,self.gl.check_vertical(chessboardMatrix,4,0))
-        self.assertEqual(True,self.gl.check_vertical(chessboardMatrix,14,18))
-        self.assertEqual(True,self.gl.check_vertical(chessboardMatrix,15,18))
-        self.assertEqual(True,self.gl.check_vertical(chessboardMatrix,18,18))
-
+        self.assertEqual(True, self.gl.check_vertical(chessboardMatrix, 0, 0))
+        self.assertEqual(True, self.gl.check_vertical(chessboardMatrix, 1, 0))
+        self.assertEqual(True, self.gl.check_vertical(chessboardMatrix, 4, 0))
+        self.assertEqual(True, self.gl.check_vertical(chessboardMatrix, 14, 18))
+        self.assertEqual(True, self.gl.check_vertical(chessboardMatrix, 15, 18))
+        self.assertEqual(True, self.gl.check_vertical(chessboardMatrix, 18, 18))
 
     def test_check_upper_diagonal(self):
         chessboardMatrix = [
@@ -83,12 +83,12 @@ class TestGameLogic(unittest.TestCase):
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-        self.assertEqual(True,self.gl.check_upper_diagonal(chessboardMatrix,0,0))
-        self.assertEqual(True,self.gl.check_upper_diagonal(chessboardMatrix,1,1))
-        self.assertEqual(True,self.gl.check_upper_diagonal(chessboardMatrix,4,4))
-        self.assertEqual(True,self.gl.check_upper_diagonal(chessboardMatrix,0,14))
-        self.assertEqual(True,self.gl.check_upper_diagonal(chessboardMatrix,1,15))
-        self.assertEqual(True,self.gl.check_upper_diagonal(chessboardMatrix,4,18))
+        self.assertEqual(True, self.gl.check_upper_diagonal(chessboardMatrix, 0, 0))
+        self.assertEqual(True, self.gl.check_upper_diagonal(chessboardMatrix, 1, 1))
+        self.assertEqual(True, self.gl.check_upper_diagonal(chessboardMatrix, 4, 4))
+        self.assertEqual(True, self.gl.check_upper_diagonal(chessboardMatrix, 0, 14))
+        self.assertEqual(True, self.gl.check_upper_diagonal(chessboardMatrix, 1, 15))
+        self.assertEqual(True, self.gl.check_upper_diagonal(chessboardMatrix, 4, 18))
 
     def test_check_lower_diagonal(self):
         chessboardMatrix = [
@@ -111,12 +111,12 @@ class TestGameLogic(unittest.TestCase):
             [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0]]
-        self.assertEqual(True,self.gl.check_lower_diagonal(chessboardMatrix,14,4))
-        self.assertEqual(True,self.gl.check_lower_diagonal(chessboardMatrix,15,3))
-        self.assertEqual(True,self.gl.check_lower_diagonal(chessboardMatrix,18,0))
-        self.assertEqual(True,self.gl.check_lower_diagonal(chessboardMatrix,14,18))
-        self.assertEqual(True,self.gl.check_lower_diagonal(chessboardMatrix,15,17))
-        self.assertEqual(True,self.gl.check_lower_diagonal(chessboardMatrix,18,14))
+        self.assertEqual(True, self.gl.check_lower_diagonal(chessboardMatrix, 14, 4))
+        self.assertEqual(True, self.gl.check_lower_diagonal(chessboardMatrix, 15, 3))
+        self.assertEqual(True, self.gl.check_lower_diagonal(chessboardMatrix, 18, 0))
+        self.assertEqual(True, self.gl.check_lower_diagonal(chessboardMatrix, 14, 18))
+        self.assertEqual(True, self.gl.check_lower_diagonal(chessboardMatrix, 15, 17))
+        self.assertEqual(True, self.gl.check_lower_diagonal(chessboardMatrix, 18, 14))
 
     def test_has_winner(self):
         chessboardMatrix = [
@@ -139,14 +139,14 @@ class TestGameLogic(unittest.TestCase):
             [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0]]
-        self.assertEqual(False,self.gl.has_winner(chessboardMatrix,10,10))
-        self.assertEqual("None",self.gl.winner)
-        self.assertEqual(False,self.gl.has_winner(chessboardMatrix,11,6))
-        self.assertEqual("None",self.gl.winner)
-        self.assertEqual(True,self.gl.has_winner(chessboardMatrix,18,0))
-        self.assertEqual("Black",self.gl.winner)
-        self.assertEqual(True,self.gl.has_winner(chessboardMatrix,18,14))
-        self.assertEqual("White",self.gl.winner)
+        self.assertEqual(False, self.gl.has_winner(chessboardMatrix, 10, 10))
+        self.assertEqual("None", self.gl.winner)
+        self.assertEqual(False, self.gl.has_winner(chessboardMatrix, 11, 6))
+        self.assertEqual("None", self.gl.winner)
+        self.assertEqual(True, self.gl.has_winner(chessboardMatrix, 18, 0))
+        self.assertEqual("Black", self.gl.winner)
+        self.assertEqual(True, self.gl.has_winner(chessboardMatrix, 18, 14))
+        self.assertEqual("White", self.gl.winner)
 
 
 if __name__ == '__main__':
